aa0bc7a9c22c22eb50218a87130098ef2864e701
Jerson Castro <jersoncastro@Jersons-MacBook-Pro.local>
2024-10-17 17:11:56 -0400

    code cleanup

--- apps/daily-words/src/main.ts
+++ apps/daily-words/src/main.ts
@@ -2,10 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-// if (process.env['NODE_ENV'] === 'production') {
-//   enableProdMode();
-// }
-
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
 );
