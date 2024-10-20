5a7b378e49feb0701e1196b29e06e0340ffeafd8
Jerson Castro <jersoncastro@Jersons-MacBook-Pro.local>
2024-10-19 21:49:03 -0400

    added atom component progress spinner

---
+++ libs/shared/ui/src/lib/atoms/progress-spinner.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'dw-progress-spinner',
+  standalone: true,
+  imports: [CommonModule],
+  template: `<p>progress-spinner works!</p>`,
+  styles: ``,
+})
+export class ProgressSpinnerComponent {}
